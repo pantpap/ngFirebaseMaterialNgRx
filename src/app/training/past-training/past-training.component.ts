104d25e5d9cc5206a4264c43fc6732ad3e868c0f
Pantelis Papasavvas <pantpap@gmail.com>
2020-12-27 00:40:01 +0200

    feat (past trainings)add material table and sorting on past trainings

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
-  styleUrls: ['./past-training.component.scss']
+  styleUrls: ['./past-training.component.scss'] 
 })
 export class PastTrainingComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
+  constructor(private trainingService: TrainingService) { }
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
+    this.dataSource.data = this.trainingService.getCompletedCancelledExercises()
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
