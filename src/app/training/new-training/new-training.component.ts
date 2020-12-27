933e0d6988a146c18cf078d1e3092d83b463c60c
Pantelis Papasavvas <pantpap@gmail.com>
2020-12-28 00:46:21 +0200

    fix(new training) change to snapshotChanges for map data to meet the model

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -5,6 +5,7 @@ import { AngularFirestore } from 'angularfire2/firestore'
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-new-training',
@@ -12,12 +13,25 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>; 
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.db.collection('availableExercises').valueChanges(); 
+    this.exercises = this.db.collection('availableExercises')
+    .snapshotChanges()
+    .pipe(map(docArray=> {
+      return docArray.map(doc => {
+        return {
+          id: doc.payload.doc.id,
+          ...doc.payload.doc.data()
+          // name: doc.payload.doc.data().name,
+          // duration: doc.payload.doc.data().duration,
+          // calories: doc.payload.doc.data().calories,
+        }
+      });
+    }));
+
     // this.exercises = this.trainingService.getAvailableExercises();
   }
 
